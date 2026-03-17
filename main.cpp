4f112a95c0477c4b99859664fd12e470790f43ad
Thomas Lieb <ascii255@users.noreply.github.com>
2026-03-17 20:35:07 +0100

    chore: enable testing and update main function for command-line test selection

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <capi>
+#include <cstdlib>
+#include <string_view>
 
-int main() {
-  capi::testing::run_unique_id_tests();
-  capi::testing::run_unique_res_tests();
-  return 0;
+int main(int argc, char** argv) {
+  const auto run_all = [] {
+    capi::testing::run_unique_id_tests();
+    capi::testing::run_unique_res_tests();
+  };
+
+  if (argc < 2) {
+    run_all();
+    return EXIT_SUCCESS;
+  }
+
+  const std::string_view selector { argv[1] };
+  if (selector == "unique_id") {
+    capi::testing::run_unique_id_tests();
+    return EXIT_SUCCESS;
+  }
+
+  if (selector == "unique_res") {
+    capi::testing::run_unique_res_tests();
+    return EXIT_SUCCESS;
+  }
+
+  if (selector == "all") {
+    run_all();
+    return EXIT_SUCCESS;
+  }
+
+  return EXIT_FAILURE;
 }
